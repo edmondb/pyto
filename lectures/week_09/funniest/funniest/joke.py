81ecd6bdb49452099cf19926ca9876df2fb41dfe
Brent Smith <brent.smith@nasa.gov>
2017-05-09 09:27:21 -0400

    packages

---
+++ lectures/week_09/funniest/funniest/joke.py
@@ -0,0 +1,34 @@
+print('''Subject: Python versus Perl: A humorous look
+From: larry (funkster@midwinter.com)
+Date: 10 Jul 1999 01:45:07 -0700
+
+This has been percolating in the back of my mind for a while.
+It's a scene from _The Empire Strikes Back_ reinterpreted to serve
+a valuable moral lesson for aspiring programmers.
+
+--
+
+EXTERIOR: DAGOBAH -- DAY
+           With Yoda strapped to his back, Luke climbs up one of
+        the many thick vines that grow in the swamp until he
+        reaches the Dagobah statistics lab. Panting heavily, he
+        continues his exercises -- grepping, installing new
+        packages, logging in as root, and writing replacements for
+        two-year-old shell scripts in Python.
+
+YODA: Code!  Yes.  A programmer's strength flows from code
+      maintainability.  But beware of Perl.  Terse syntax... more
+      than one way to do it...  default variables.  The dark side
+      of code maintainability are they.  Easily they flow, quick
+      to join you when code you write.  If once you start down the
+      dark path, forever will it dominate your destiny, consume
+      you it will.
+
+LUKE: Is Perl better than Python?
+
+YODA: No... no... no.  Quicker, easier, more seductive.
+
+LUKE: But how will I know why Python is better than Perl?
+
+YODA: You will know.  When your code you try to read six months
+      from now.''')
